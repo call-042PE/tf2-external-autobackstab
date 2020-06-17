afad3f2c4d205d10928e8627d8823e83b943a1e6
Mr. Anderson <44845040+call-042PE@users.noreply.github.com>
2020-06-17 09:51:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "EditMemory.h"
 
+DWORD m_hActiveWeapon = 0x0DB8;
+DWORD dwEntityList = 0xC490B4;
+DWORD m_bReadyToBackstab = 0xC64;
+
 int main()
 {
 	int procId = GetProcId(L"hl2.exe");
@@ -8,18 +12,18 @@ int main()
 	HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
 	while (true)
 	{
-		DWORD ptrBase = moduleBase + 0x00C37ADC;
-		std::vector<unsigned int> m_CanBackStab = { 0x0,0x14,0x4,0x1EC,0x6F4 };
-		uintptr_t BackStab = FindDynamicAddr(handle, ptrBase, m_CanBackStab);
-		int CanBackStab = editmemory::ReadMem<int>(handle, LPVOID(BackStab));
-		std::cout << CanBackStab << std::endl;
-		if (CanBackStab == 256)
+		DWORD localPlayer = editmemory::ReadMem<DWORD>(handle, LPVOID(moduleBase + 0xC3B590));
+		DWORD active_weapon = editmemory::ReadMem<DWORD>(handle, LPVOID(localPlayer + m_hActiveWeapon));
+		DWORD current_weapon = editmemory::ReadMem<DWORD>(handle, LPVOID(moduleBase + dwEntityList + (((active_weapon & 0xFFF) - 1) * 0x10)));
+		bool bReadyToBackstab = editmemory::ReadMem<bool>(handle, LPVOID(current_weapon + m_bReadyToBackstab));
+		std::cout << bReadyToBackstab << std::endl;
+		if (bReadyToBackstab == 1)
 		{
 			//dwForceAttack
-			editmemory::WriteMem<int>(handle, LPVOID(moduleBase + 0xC692EC), 5);
+			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
 			Sleep(20);
 			//dwForceAttack
-			editmemory::WriteMem<int>(handle, LPVOID(moduleBase + 0xC692EC), 4);
+			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 			Sleep(10);
 		}
 	}
